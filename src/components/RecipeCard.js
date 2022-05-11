1bba260df0ad4f35bcf6eee32f6c7b226c31f63c
Christian <christian.digiorno@tiendanube.com>
2022-05-11 20:56:20 -0300

    Style improvements

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,25 +1,27 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material'
-import React from 'react'
+import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import React from 'react';
+import '../styles/index.css';
 
-export const RecipeCard = ({ id, title, image, prepTime }) => {
+export const RecipeCard = ({ id, title, image, category, score, difficulty }) => {
   return (
     <Grid item xs={10} sm={4} lg={3} align="center">
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image={image}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
+      <Card sx={{ maxWidth: 345, height: "100%" }}>
+        <div style={{ height: "85%" }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={image}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <p className="recipe-card-field"><strong>Categoría:</strong> {category}</p>
+            <p className="recipe-card-field"><strong>Calificación:</strong> {score}</p>
+            <p className="recipe-card-field"><strong>Dificultad:</strong> {difficulty}</p>
+          </CardContent>
+        </div>
         <CardActions style={{justifyContent: 'center'}}>
           <Button href={`recipes/${id}`} size="large">Ver Más</Button>
         </CardActions>
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -9,7 +9,16 @@ export const RecipeList = ({recipes}) => {
   return (
     <Box sx={{ flexGrow: 1, margin:"20px" }} >
       <Grid container spacing={2} justifyContent="center">
-        {recipes.map(item => <RecipeCard id={item.id} title={item.title} image={item.images[0]} prepTime={item.prep_time}/>)}
+        {recipes.map(item =>
+          <RecipeCard
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            image={item.images[0]}
+            category={item.category}
+            score={item.score}
+            difficulty={item.difficulty}
+          />)}
       </Grid>
     </Box>
   )
